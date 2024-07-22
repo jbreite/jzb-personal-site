2f512dc0f2a3c53b2e608906a82c09519121c855
jbreite <joshbreite@gmail.com>
2024-07-22 10:03:26 -0400

    New icon

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Josh Breite",
   description: "Josh Breite's personal website",
+  icons: {
+    icon: "/simpleIcon.png",
+  },
 };
 
 export default function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,15 @@ import FarcasterLogo from "../public/farcasterLogo.svg";
 import LinkedinLogo from "../public/linkedinLogo.svg";
 import SubstackLogo from "../public/substackLogo.svg";
 
+const FireLink = "https://www.joinfire.xyz/";
+const AtomicLink = "https://atomic.vc/";
+const PogoLink = "https://www.joinpogo.com/";
+const TwitterLink = "https://x.com/joshbreite";
+const EmailLink = "mailto:joshbreite@gmail.com";
+const FarcasterLink = "https://warpcast.com/joshbreite.eth";
+const LinkedInLink = "https://www.linkedin.com/in/josh-breite-0ba696173/";
+const SubstackLink = "https://joshbreite.substack.com/";
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-start py-6 px-6 sm:py-16">
@@ -17,22 +26,20 @@ export default function Home() {
         <div className="flex flex-col gap-y-4 py-6 ">
           <p>I live in New York. Currently exploring new opportunities.</p>
           <p>
-            I co-founded{" "}
-            <TextLink href="http://joinfire.xyz" label="Fire" external /> a a
+            I co-founded <TextLink href={FireLink} label="Fire" external /> a a
             security tool to help users protect funds while interacting with
             crypto. We had 60k+ users who downloaded our extension, while
             protecting over $750m in assets.
           </p>
           <p>
             Before that, I worked for{" "}
-            <TextLink label="Atomic" href="http://atomic.vc" />, starting as an
-            intern and then to product manager in residence, where I worked on
-            many different types of crypto products.
+            <TextLink label="Atomic" href={AtomicLink} />, starting as an intern
+            and then to product manager in residence, where I worked on many
+            different types of crypto products.
           </p>
           <p>
-            I also worked at{" "}
-            <TextLink href="http://joinpogo.com" label="Pogo" /> as an intern
-            where I worked on product (and other things).
+            I also worked at <TextLink href={PogoLink} label="Pogo" /> as an
+            intern where I worked on product (and other things).
           </p>
           <p>
             I graduated from Northwestern with a major in Learning and
@@ -41,33 +48,25 @@ export default function Home() {
 
           <p>
             Reach me at either{" "}
-            <TextLink href="https://x.com/joshbreite" label="@joshbreite" /> or{" "}
-            <TextLink
-              href="mailto:joshbreite@gmail.com"
-              label="joshbreite@gmail.com"
-            />
-            .
+            <TextLink href={TwitterLink} label="@joshbreite" /> or{" "}
+            <TextLink href={EmailLink} label="joshbreite@gmail.com" />.
           </p>
         </div>
 
         <div className="flex  flex-row gap-x-4 gap-y-2 flex-wrap">
+          <ButtonLink href={TwitterLink} label="Twitter" image={TwitterLogo} />
           <ButtonLink
-            href="https://x.com/joshbreite"
-            label="Twitter"
-            image={TwitterLogo}
-          />
-          <ButtonLink
-            href="https://warpcast.com/joshbreite.eth"
+            href={FarcasterLink}
             label="Farcaster"
             image={FarcasterLogo}
           />
           <ButtonLink
-            href="https://www.linkedin.com/in/josh-breite-0ba696173/"
+            href={LinkedInLink}
             label="LinkedIn"
             image={LinkedinLogo}
           />
           <ButtonLink
-            href="https://joshbreite.substack.com/"
+            href={SubstackLink}
             label="Substack"
             image={SubstackLogo}
           />
